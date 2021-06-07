fc8487304b7824cc824e3ef3f6b6d26fa7796f95
raysk4ever <raysk7161@gmail.com>
2021-06-07 15:57:05 +0530

    added DOM

--- js/objects.js
+++ js/objects.js
@@ -36,36 +36,89 @@
 
 
 
-let data = {
-    name: "amit",
-    age: 20,
-    country: "India",
-    address: {
-        city: 'Rudrapur',
-        state: 'Uttrakhand'
-    },
-    a: {
-        b: {
-            b: {
-                e: [{a: '12'}]
-            }
-        }
-    }
-}
+// let data = {
+//     name: "amit",
+//     age: 20,
+//     country: "India",
+//     address: {
+//         city: 'Rudrapur',
+//         state: 'Uttrakhand'
+//     },
+//     a: {
+//         b: {
+//             b: {
+//                 e: [{a: '12'}]
+//             }
+//         }
+//     }
+// }
 // let newDataObj = data
 
 // // copy/clone obj to newone (doesn't make deepcopy)
-let newDataObj = Object.assign({}, data) // copy by another obj => store in diff ref other then obj
+// let newDataObj = Object.assign({}, data) // copy by another obj => store in diff ref other then obj
 
-// newDataObj.age = 200
-// newDataObj.address.city = 'Agra'
-console.log({data, newDataObj})
-
-
-const str = JSON.stringify(data) // obj ==> string (Valid Json)
-const newDataObj = JSON.parse(str) // string (valied json) ==> obj
+// // newDataObj.age = 200
+// // newDataObj.address.city = 'Agra'
+// console.log({data, newDataObj})
 
 
+// const str = JSON.stringify(data) // obj ==> string (Valid Json)
+// const newDataObj = JSON.parse(str) // string (valied json) ==> obj
 
 
+
+// Destructing ==> Object, Array
+
+const data = {
+    todo: 'Lern JS',
+    rating: 10,
+    timing: '4:PM',
+    other: {
+        one: 'more'
+    }
+}
+
+// console.log(data.todo)
+// console.log(data.timing)
+
+const todo = 'other data'
+// const { timing, todo : otherOther, rating: r, country='INDIA' } = data // object order
+
+// console.log(otherOther)
+// console.log(timing)
+// console.log(r)
+// console.log(country)
+
+// spread/rest opreator
+
+const {timing, ...rest} = data //rest
+
+
+
+// let newDataObj = Object.assign({}, data)
+
+let cloneData = { ...data } //spread
+// console.log(cloneData)
+// 
+cloneData.todo = 'two'
+cloneData.other.one = 'two'
+
+
+const a = {
+    a: '1',
+    b: 'asd'
+}
+const b = {
+    b: 2
+}
+
+const c = {
+    c: 3
+}
+
+const d ={
+    ...a,
+    ...b
+}
+console.log(d)
 
